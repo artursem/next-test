43c3da5a8f12d18ca1f18ec2c235f3a729fc9a0b
artursem <artur.semeniuk@gmail.com>
2022-05-22 21:49:45 +0200

    course app

--- README.md
+++ README.md
@@ -1,25 +1,13 @@
-# Next.js + Jest
+# Popular Concert Venue
 
-This example shows how to configure Jest to work with Next.js.
+### An app to support the Udemy course [Testing Next.js Apps](https://www.udemy.com/course/nextjs-testing/)
 
-This includes Next.js' built-in support for Global CSS, CSS Modules and TypeScript.
+## Installation
 
-## How to Use
+1. Run `npm install`
+1. Run `cp .env.local_template .env.local`
+1. Add a long, hard-to-guess strings as the values for `NEXTAUTH_SECRET` and `REVALIDATION_SECRET` in _.env.local_
 
-Quickly get started using [Create Next App](https://github.com/vercel/next.js/tree/canary/packages/create-next-app#readme)!
+## Running the App
 
-In your terminal, run the following command:
-
-```bash
-npx create-next-app --example with-jest with-jest-app
-# or
-yarn create next-app --example with-jest with-jest-app
-# or
-pnpm create next-app -- --example with-jest with-jest-app
-```
-
-## Run Jest Tests
-
-```bash
-npm test
-```
+Run `npm run dev`. The app will be found at [http://localhost:3000]
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,41 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppInitialProps } from "next/app";
+import type { Router } from "next/router";
+import type { NextComponentType } from "next/types";
+import { SessionProvider } from "next-auth/react";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Layout } from "@/components/_common/Layout";
+import { Auth } from "@/components/auth/Auth";
+import { theme } from "@/lib/theme";
+
+// note: need to use `type` (not `interface`) because not all
+// NextComponentType members are statically typed
+// Generally prefer interface for better error messages
+// (see https://www.typescripttutorial.net/typescript-tutorial/typescript-extend-interface/)
+type ComponentWithAuth = NextComponentType & { auth: boolean };
+interface AppPropsWithAuth extends AppInitialProps {
+  Component: ComponentWithAuth;
+  router: Router;
+}
+
+export default function PopularMusicVenue({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithAuth) {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          {/* adapted from https://next-auth.js.org/getting-started/client#custom-client-session-handling */}
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
+            <Component {...pageProps} />
+          )}{" "}
+        </Layout>
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
