848e20e6164d8a9310a35ac2765d76f5d5f607f6
artursem <artur.semeniuk@gmail.com>
2022-05-23 21:19:26 +0200

    test heading and image

--- __tests__/ui/home.test.tsx
+++ __tests__/ui/home.test.tsx
@@ -3,6 +3,13 @@ import Home from '@/pages/index';
 
 test('Page has correct heading and image', () => {
 	render(<Home />);
-	const heading = screen.getByRole('heading', /welcome to popular concert venue/i);
+	const heading = screen.getByRole('heading', {
+		name: /welcome to popular concert venue/i,
+	});
 	expect(heading).toBeInTheDocument();
+
+	const image = screen.getByRole('img', {
+		name: 'Concert goer with hands in the shape of a heart',
+	});
+	expect(image).toBeInTheDocument();
 });
