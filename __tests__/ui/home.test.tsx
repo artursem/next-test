6c94bae865a4d6dd9dd7cd333dbb8749cc4550b5
artursem <artur.semeniuk@gmail.com>
2022-05-23 21:16:48 +0200

    prettier, first test

--- __tests__/ui/home.test.tsx
+++ __tests__/ui/home.test.tsx
@@ -1,14 +1,8 @@
 import { render, screen } from '@testing-library/react';
-
 import Home from '@/pages/index';
 
-describe('Home', () => {
-	// it('renders a heading', () => {
+test('Page has correct heading and image', () => {
 	render(<Home />);
-
-	const heading = screen.getByRole('heading', {
-		name: /welcome to next\.js!/i,
-	});
-
+	const heading = screen.getByRole('heading', /welcome to popular concert venue/i);
 	expect(heading).toBeInTheDocument();
 });
