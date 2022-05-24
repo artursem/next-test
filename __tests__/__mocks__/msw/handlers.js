e4184a8bb4111d722c54f78a59e0c24076bae9e6
artursem <artur.semeniuk@gmail.com>
2022-05-24 19:33:38 +0200

    handler setup

--- __tests__/__mocks__/msw/handlers.js
+++ __tests__/__mocks__/msw/handlers.js
@@ -1,3 +1,10 @@
 import { rest } from 'msw';
 
-export const handlers = [];
+import { readFakeData } from '@/__tests__/__mocks__/fakeData';
+
+export const handlers = [
+	rest.get('http://localhost:3000/api/shows/:showId', async (req, res, ctx) => {
+		const { fakeShows } = await readFakeData();
+		return res(ctx.json({ show: fakeShows[0] }));
+	}),
+];
