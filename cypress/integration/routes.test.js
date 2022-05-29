e36e4a6336e4fec8f02f18e0266adf46d39ff700
artursem <artur.semeniuk@gmail.com>
2022-05-29 20:46:14 +0200

    test bands route w/ cypress

--- cypress/integration/routes.test.js
+++ cypress/integration/routes.test.js
@@ -3,3 +3,9 @@ it('displays correct heading in shows route', () => {
 	cy.findByRole('button', { name: /shows/i }).click();
 	cy.findByRole('heading', { name: /upcoming shows/i }).should('exist');
 });
+
+it('displays correct heading in bands route', () => {
+	cy.visit('/');
+	cy.findByRole('button', { name: /bands/i }).click();
+	cy.findByRole('heading', { name: /Our Illustrious Performers/i }).should('exist');
+});
