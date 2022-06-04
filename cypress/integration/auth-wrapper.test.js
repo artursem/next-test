e10c25a2101982626d6bc2e1abc797bf9171a619
artursem <artur.semeniuk@gmail.com>
2022-06-04 12:10:48 +0200

    test auth failure path

--- cypress/integration/auth-wrapper.test.js
+++ cypress/integration/auth-wrapper.test.js
@@ -33,3 +33,52 @@ it('runs auth flow for successful login to protected reservations page', () => {
 	// sign out not in the page
 	cy.findByRole('button', { name: /sing in/i }).should('not.exist');
 });
+
+it('runs auth failure flow', () => {
+	// reset and go to user
+	cy.task('db:reset').visit('/user');
+
+	// check for sign in form
+	cy.findByRole('heading', { name: /sign in to your account/i }).should('exist');
+
+	// no option to purchase tickets
+	cy.findByRole('heading', { name: /your tickets/i }).should('not.exist');
+
+	// find form and enter WRONG credentials
+	cy.findByLabelText(/email address/i)
+		.clear()
+		.type('wrong@email');
+
+	cy.findByLabelText(/password/i)
+		.clear()
+		.type('wrong');
+
+	// submit the form
+	cy.findByRole('main').within(() => {
+		cy.findByRole('button', { name: /sign in/i }).click();
+	});
+
+	// sign in failed?
+	cy.findByText(/sign in failed/i).should('exist');
+
+	// no option to purchase tickets
+	cy.findByRole('heading', { name: /your tickets/i }).should('not.exist');
+
+	// enter sign in credentials
+	cy.findByLabelText(/email address/i)
+		.clear()
+		.type(Cypress.env('TEST_USER_EMAIL'));
+
+	cy.findByLabelText(/password/i)
+		.clear()
+		.type(Cypress.env('TEST_PASSWORD'));
+
+	// submit the form
+	cy.findByRole('main').within(() => {
+		cy.findByRole('button', { name: /sign in/i }).click();
+	});
+
+	// check for purchase button and band name
+	cy.findByRole('button', { name: /purchase/i }).should('exist');
+	cy.findByRole('heading', { name: /wandering bunnies/i }).should('exist');
+});
