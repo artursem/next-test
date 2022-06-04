89f311a919d9cdaa426a5265f4a7d254d3ee1b74
artursem <artur.semeniuk@gmail.com>
2022-06-04 13:04:12 +0200

    add sign in command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,27 @@
-import "@testing-library/cypress/add-commands";
+import '@testing-library/cypress/add-commands';
 
-Cypress.Commands.add("resetDbAndIsrCache", () => {
-  cy.task("db:reset");
-  const secret = Cypress.env("REVALIDATION_SECRET");
-  cy.request("GET", `/api/revalidate?secret=${secret}`);
+Cypress.Commands.add('resetDbAndIsrCache', () => {
+	cy.task('db:reset');
+	const secret = Cypress.env('REVALIDATION_SECRET');
+	cy.request('GET', `/api/revalidate?secret=${secret}`);
+});
+
+Cypress.Commands.add('signIn', (email, password) => {
+	cy.visit('/auth/signin');
+	// enter sign in credentials
+	cy.findByLabelText(/email address/i)
+		.clear()
+		.type(email);
+
+	cy.findByLabelText(/password/i)
+		.clear()
+		.type(password);
+
+	// submit the form
+	cy.findByRole('main').within(() => {
+		cy.findByRole('button', { name: /sign in/i }).click();
+	});
+
+	// welcome message
+	cy.findByRole('heading', { name: /welcome/i }).should('exist');
 });
