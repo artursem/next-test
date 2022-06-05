09192da8d12955a89f259df3e4c0b471797ef74e
artursem <artur.semeniuk@gmail.com>
2022-06-05 09:20:54 +0200

    polyfill necessary for jsdom test environment

--- jest.setup.js
+++ jest.setup.js
@@ -3,13 +3,24 @@
 
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+import '@testing-library/jest-dom/extend-expect';
+
+// polyfill necessary for jsdom test environment
+import { TextDecoder, TextEncoder } from 'util';
 
 // src/setupTests.js
-import { server } from "./__tests__/__mocks__/msw/server";
+import { server } from './__tests__/__mocks__/msw/server';
+import { resetDB } from '@/__tests__/__mocks__/db/utils/reset-db';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
 
+beforeEach(async () => {
+	await resetDB();
+});
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
 afterEach(() => server.resetHandlers());
