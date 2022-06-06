05660aa6b313c39b9ea40ff6b90f49b3de65301e
artursem <artur.semeniuk@gmail.com>
2022-06-06 17:44:39 +0200

    test api show id

--- __tests__/api/shows.test.js
+++ __tests__/api/shows.test.js
@@ -2,8 +2,9 @@ import { testApiHandler } from 'next-test-api-route-handler';
 
 import { readFakeData } from '@/__tests__/__mocks__/fakeData';
 import showsHandler from '@/pages/api/shows/index';
+import showIdHandler from '@/pages/api/shows/[showId]';
 
-test('/api/shows returns shows from the db', async () => {
+test('GET /api/shows returns shows from the db', async () => {
 	await testApiHandler({
 		handler: showsHandler,
 		test: async ({ fetch }) => {
@@ -13,6 +14,21 @@ test('/api/shows returns shows from the db', async () => {
 
 			const { fakeShows } = await readFakeData();
 			expect(json).toEqual({ shows: fakeShows });
+		},
+	});
+});
+
+test('GET /api/shows/[showid] returns the data for the correct show ID', async () => {
+	await testApiHandler({
+		handler: showIdHandler,
+		paramsPatcher: (params) => {
+			// eslint-disable-next-line no-param-reassign
+			params.showId = 0;
+		},
+		test: async ({ fetch }) => {
+			const res = await fetch({ method: 'GET' });
+			expect(res.status).toBe(200);
+			const json = await res.json();
 		},
 	});
 });
