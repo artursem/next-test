19304411d21b21b706d4f3d1b5fb6c3242e55ccb
artursem <artur.semeniuk@gmail.com>
2022-06-24 20:00:49 +0200

    test POST reservations return 401 when unauth

--- __tests__/api/reservations.test.ts
+++ __tests__/api/reservations.test.ts
@@ -1,8 +1,10 @@
 import { testApiHandler } from 'next-test-api-route-handler';
 import userReservationsHandler from '@/pages/api/users/[userId]/reservations';
 import reservationHandler from '@/pages/api/reservations/[reservationId]';
+import { validateToken } from '@/lib/auth/utils';
 
 jest.mock('@/lib/auth/utils');
+const mockValidateToken = validateToken as jest.Mock;
 
 test('POST /api/reservations creates a reservation', async () => {
 	await testApiHandler({
@@ -39,6 +41,32 @@ test('POST /api/reservations creates a reservation', async () => {
 			expect(res.status).toEqual(200);
 			const json = await res.json();
 			expect(json.userReservations).toHaveLength(3);
+		},
+	});
+});
+
+test('POST /api/reservations/[reservationId] returns 401 status when not authorized', async () => {
+	mockValidateToken.mockResolvedValue(false);
+	await testApiHandler({
+		handler: reservationHandler,
+		paramsPatcher: (params) => {
+			// eslint-disable-next-line no-param-reassign
+			params.reservationId = 12345;
+		},
+		test: async ({ fetch }) => {
+			const res = await fetch({
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json', // Must use correct content type
+				},
+				body: JSON.stringify({
+					seatCount: 5,
+					userId: 1,
+					showId: 0,
+				}),
+			});
+			// expect unauthorized
+			expect(res.status).toBe(401);
 		},
 	});
 });
